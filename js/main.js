b89809076168ec8bbcfd7730a435488a5e7232d1
oliviaarden <mac99@SRMAC02.local>
2016-04-05 15:10:51 -0400

    fixing js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ var $engage = $('.engage');
 var $btn = $('.btn');
 
 $('svg').on('click', function (e) {
-  $('.lines').toggleClass('is-pressed');
   $('.engage').toggleClass('engaged');
   $btn.toggleClass('button');
   $('svg').toggleClass('is-pressed');
